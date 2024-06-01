9b288cc86ab492c86a156a642bcec8a7b3d0dd70
bravetroops <78992609+timeFade@users.noreply.github.com>
2024-06-01 16:49:54 +0800

    Update trim_history_scheduler.h

--- db/trim_history_scheduler.h
+++ db/trim_history_scheduler.h
@@ -15,7 +15,15 @@
 namespace ROCKSDB_NAMESPACE {
 
 class ColumnFamilyData;
+// TrimHistoryScheduler 类，用于管理需要对历史数据进行修剪的列族（Column Family）。以下是该类的主要功能：
 
+// ScheduleWork 方法：用于将需要进行历史数据修剪的列族添加到 FIFO 队列中。
+
+// TakeNextColumnFamily 方法：用于从 FIFO 队列中取出下一个需要进行历史数据修剪的列族。
+
+// Empty 方法：用于检查 FIFO 队列是否为空。
+
+Clear 方法：用于清空 FIFO 队列中的所有列族。
 // Similar to FlushScheduler, TrimHistoryScheduler is a FIFO queue that keeps
 // track of column families whose flushed immutable memtables may need to be
 // removed (aka trimmed). The actual trimming may be slightly delayed. Due to
